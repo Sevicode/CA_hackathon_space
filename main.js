ed70714e287be82c8cbba1ca91d80f630923489a
ChrisWhite12 <chris_white_12@hotmail.com>
2020-09-21 14:46:59 +1000

    added getPlanetInfo

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 let sat_btn = document.getElementById("saturn_btn")
-
+let info_btn = document.getElementById("info_btn")
+let query_form = document.getElementById("query_form")
+let planet_card = document.getElementById("planet_card")
 
 function getPlanetImg(){
+    planet = 'saturn'
     topResults = {}
-    fetch(`https://images-api.nasa.gov/search?q=saturn&media_type=image`)
+    fetch(`https://images-api.nasa.gov/search?q=${planet}&media_type=image`)
     .then(res => res.json())
     .then( data => {
         // console.log(data)
@@ -19,8 +22,44 @@ function getPlanetImg(){
     )
     .catch(err => alert(err))
 
-    
+    return topResults
+}
+
+// function getWikiInfo(){
+
+//     planet = 'saturn'
+//     wiki_info = {}
+//     fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${planet}&format=json&origin=*`)
+//     .then(res => res.json())
+//     .then(data => {
+//         first_result = data["query"]["search"][0]
+//         console.log(first_result)
+//     })
+// }
+
+function getPlanetInfo(event){
+    event.preventDefault()
+    planet = query_form.planet.value
+    planetUl = document.createElement('ul')
+    planet_card.appendChild(planetUl)
+    // planet = 'mars'                     //default mars
+    // find the planet from the search query
+    // or pass the planet variable when a image is selected
+
+    planetInfo = {}
+    fetch(`https://api.le-systeme-solaire.net/rest.php/bodies?filter%5B%5D=englishName%2Ceq%2C${planet}`)           //getting the planet information from API
+    .then(res => res.json())
+    .then(data => {
+        planetResult = data["bodies"][0]
+        console.log(planetResult)
+        planetUl.innerHTML = `<li>Name: ${planetResult["englishName"]}</li><li> Radius: ${planetResult["meanRadius"]} km</li><li>Orbit Radius: ${planetResult["semimajorAxis"]} km</li><li> Gravity: ${planetResult["gravity"]} m/s^2</li>`
+    })
+
+
+
 }
 
 
 sat_btn.addEventListener('click', getPlanetImg)
+info_btn.addEventListener('click', getPlanetInfo)
+query_form.addEventListener('submit', getPlanetInfo)
